44462f179f37a07f3724e45457f70db4b70dc063
Jarod Wilson <jaroddwilson2000@gmail.com>
2022-09-15 11:36:10 -0700

    Added functionality with Expo, base login/logout pages

--- App.tsx
+++ App.tsx
@@ -1,18 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './components/login';
+import Signup from './components/signup';
+import Dashboard from './components/dashboard';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={
+          {title: 'Login'},
+          {headerLeft: null} 
+        }
+      />
+      <Stack.Screen 
+       name="Dashboard" 
+       component={Dashboard} 
+       options={
+         { title: 'Dashboard' },
+         {headerLeft: null} 
+       }
+      />
+    </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+// database/firebaseDb.js
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4zL6Uu8eQ1fH8AzNxR-zd5MUVxzKnZkU",
+    authDomain: "reactnativefirebase-00000.firebaseapp.com",
+    databaseURL: "https://reactnativefirebase-00000.firebaseio.com/",
+    projectId: "reactnativefirebase-00000",
+    storageBucket: "reactnativefirebase-00000.appspot.com",
+    messagingSenderId: "000000000000000",
+    appId: "1:000000000000000:web:000000000000000"
+};
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
