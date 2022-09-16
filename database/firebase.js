fec5b2035b764b39bf84fd1723815c523f3c4606
OscarDU123 <oscar97ulloa@gmail.com>
2022-09-16 13:22:12 -0700

    Fixed firebase.auth error

--- database/firebase.js
+++ database/firebase.js
@@ -1,6 +1,28 @@
-// database/firebaseDb.js
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4zL6Uu8eQ1fH8AzNxR-zd5MUVxzKnZkU",
+    authDomain: "reactnativefirebase-00000.firebaseapp.com",
+    databaseURL: "https://reactnativefirebase-00000.firebaseio.com/",
+    projectId: "reactnativefirebase-00000",
+    storageBucket: "reactnativefirebase-00000.appspot.com",
+    messagingSenderId: "000000000000000",
+    appId: "1:000000000000000:web:000000000000000"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+
+
+// database/firebaseDb.js
+//import { initializeApp } from 'firebase/app';
+
+/*
 const firebaseConfig = {
     apiKey: "AIzaSyB4zL6Uu8eQ1fH8AzNxR-zd5MUVxzKnZkU",
     authDomain: "reactnativefirebase-00000.firebaseapp.com",
@@ -12,4 +34,5 @@ const firebaseConfig = {
 };
 const firebase = initializeApp(firebaseConfig);
 
-export default firebase;
+export default firebase 
+*/
