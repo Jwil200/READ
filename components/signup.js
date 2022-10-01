ef2a3db753a26fc2ae1e2445a01be2fe0b5eadad
OscarDU123 <oscar97ulloa@gmail.com>
2022-09-30 18:44:59 -0700

    New Sign-up page

--- components/signup.js
+++ components/signup.js
@@ -1,94 +1,138 @@
 // components/signup.js
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicator } from 'react-native';
+//import React, { Component } from 'react';
+import { useNavigation } from '@react-navigation/core'
+import React, { useEffect, useState } from 'react'
+//import { auth } from '../firebase'
+import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicator, Image, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { SocialIcon } from 'react-native-elements';
 import firebase from '../database/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-export default class Signup extends Component {
-  
-  constructor() {
-    super();
-    this.state = { 
-      displayName: '',
-      email: '', 
-      password: '',
-      isLoading: false
-    }
-  }
-  updateInputVal = (val, prop) => {
-    const state = this.state;
-    state[prop] = val;
-    this.setState(state);
-  }
+const OrangeButton = ({ onPress, title }) => (
+  <TouchableOpacity onPress={registerUser}>
+    <LinearGradient
+      colors={["orange","#e65c00"]}
+      style={styles.appButtonContainer}
+    >  
+      <Text style={styles.appButtonText}>{title}</Text>
+    </LinearGradient>
+  </TouchableOpacity>
+);
+
+const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayname, setName] = useState('')
+
+  const navigation = useNavigation()
+
   registerUser = () => {
-    if(this.state.email === '' && this.state.password === '') {
-      Alert.alert('Enter details to signup!')
-    } else {
-      this.setState({
-        isLoading: true,
-      })
+    if(email === '' && password === '') {
+      Alert.alert('Enter your details to sign up!')
+    }
+    else {
       firebase
       .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then((res) => {
-        res.user.updateProfile({
-          displayName: this.state.displayName
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredenstials =>{
+        const user = userCredenstials.user;
+
+        //displays user email to terminal for testing
+        console.log(user.email)
+        
+        userCredenstials.user.updateProfile({// needs testing
+           displayName: displayname
         })
-        console.log('User registered successfully!')
-        this.setState({
-          isLoading: false,
-          displayName: '',
-          email: '', 
-          password: ''
-        })
-        this.props.navigation.navigate('Login')
+        
+        
+        navigation.navigate('Login')
+        //displays username to terminal for testing
+        console.log(user.displayName)
       })
-      .catch(error => this.setState({ errorMessage: error.message }))      
-    }
+      .catch(error => alert(error.message))
+    } 
   }
-  render() {
-    if(this.state.isLoading){
-      return(
-        <View style={styles.preloader}>
-          <ActivityIndicator size="large" color="#9E9E9E"/>
-        </View>
-      )
-    }    
-    return (
+  
+
+  return (//Each component functionality
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      
       <View style={styles.container}>  
+        <Image source={require('../assets/read-logo.png')} style={styles.logo} />
         <TextInput
           style={styles.inputStyle}
           placeholder="Name"
-          value={this.state.displayName}
-          onChangeText={(val) => this.updateInputVal(val, 'displayName')}
+          placeholderTextColor="blue"
+          color= 'blue'
+          value={displayname}
+          onChangeText={text => setName(text)}
         />      
         <TextInput
           style={styles.inputStyle}
           placeholder="Email"
-          value={this.state.email}
-          onChangeText={(val) => this.updateInputVal(val, 'email')}
+          placeholderTextColor="green"
+          color= 'green'
+          value={email}
+          onChangeText={text => setEmail(text)}
         />
         <TextInput
           style={styles.inputStyle}
           placeholder="Password"
-          value={this.state.password}
-          onChangeText={(val) => this.updateInputVal(val, 'password')}
+          placeholderTextColor="orange"
+          color= 'orange'
+          value={password}
+          onChangeText={text => setPassword(text)}
           maxLength={15}
           secureTextEntry={true}
         />   
-        <Button
-          color="#3740FE"
-          title="Signup"
-          onPress={() => this.registerUser()}
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="(Birthday would go here?)"
+          placeholderTextColor="blue"
+          color= 'blue'
+        /> 
+        <View style={styles.screenContainer}>
+          <OrangeButton 
+          title="Create an Account" 
+          size="sm" 
+          onPress={() => registerUser()}
         />
+        </View>
+
+        <Text style={{textAlign: 'center', bottom: 20}}>
+            Or sign up with:
+          </Text>
+        
+        <SocialIcon
+        title='Sign Up With Facebook'
+        button type='facebook'
+        style={{bottom: 20}}
+        />
+
+        <SocialIcon
+        title='Sign Up With Google'
+        button type='google'
+        style={{bottom: 20}}
+        />
+
         <Text 
           style={styles.loginText}
-          onPress={() => this.props.navigation.navigate('Login')}>
-          Already Registered? Click here to login
+          onPress={() => navigation.navigate('Login')}>
+          Already registered? Tap here to Log In
         </Text>                          
       </View>
-    );
-  }
+    </KeyboardAvoidingView>
+  );
+
 }
+  
+export default Signup
+
+//Style sheet to customize indiviual parts.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -96,19 +140,21 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     padding: 35,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   inputStyle: {
     width: '100%',
     marginBottom: 15,
     paddingBottom: 15,
-    alignSelf: "center",
+    alignSelf: "stretch",
     borderColor: "#ccc",
-    borderBottomWidth: 1
+    borderBottomWidth: 1,
+    bottom: 80,
+    fontSize: 18
   },
   loginText: {
     color: '#3740FE',
-    marginTop: 25,
+    marginBottom: 20,
     textAlign: 'center'
   },
   preloader: {
@@ -120,5 +166,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff'
-  }
-});
+  },
+  logo: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'contain',
+    left: 20,
+    bottom: 60,
+  },
+
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 100,
+    paddingVertical: 15,
+    paddingHorizontal: 12,
+    bottom: 60,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+})
