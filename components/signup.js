b0655e4d4719c058ad4bd0faff510da1ec606b3a
grapes-cr <caitlinjrafael@gmail.com>
2022-09-30 20:03:15 -0400

    Updated Log In screen styling, added a styles.js file

--- components/signup.js
+++ components/signup.js
@@ -1,8 +1,9 @@
 // components/signup.js
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicator, Image, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, Alert, ActivityIndicator, Image, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { SocialIcon } from 'react-native-elements';
+import styles from './styles'
 import firebase from '../database/firebase';
 
 const OrangeButton = ({ onPress, title }) => (
@@ -128,69 +129,9 @@ export default class Signup extends Component {
         <Text 
           style={styles.loginText}
           onPress={() => this.props.navigation.navigate('Login')}>
-          Already registered? Tap here to Log In
+          Already registered? Tap here to log in
         </Text>                          
       </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: 35,
-    backgroundColor: '#fff',
-  },
-  inputStyle: {
-    width: '100%',
-    marginBottom: 15,
-    paddingBottom: 15,
-    alignSelf: "stretch",
-    borderColor: "#ccc",
-    borderBottomWidth: 1,
-    bottom: 80,
-    fontSize: 18
-  },
-  loginText: {
-    color: '#3740FE',
-    marginBottom: 20,
-    textAlign: 'center'
-  },
-  preloader: {
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff'
-  },
-  logo: {
-    flex: 1,
-    width: null,
-    height: null,
-    resizeMode: 'contain',
-    left: 20,
-    bottom: 60,
-  },
-
-  appButtonContainer: {
-    elevation: 8,
-    backgroundColor: "#009688",
-    borderRadius: 100,
-    paddingVertical: 15,
-    paddingHorizontal: 12,
-    bottom: 60,
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: "#fff",
-    fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase"
-  }
-
-});
