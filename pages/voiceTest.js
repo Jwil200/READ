6863e1efe2251969980e104a10c8aa4e08df939c
Jarod Wilson <jaroddwilson2000@gmail.com>
2022-10-06 11:28:07 -0700

    no current fix for null object issue

--- pages/voiceTest.js
+++ pages/voiceTest.js
@@ -1,60 +1,70 @@
 import React, { Component, useEffect, useState, useRef } from 'react';
 import { StyleSheet, View, Text, FlatList, ScrollView, PermissionsAndroid } from 'react-native';
-import LiveAudioStream from 'react-native-live-audio-stream';
+import { Button } from "@rneui/themed";
+import AudioRecord from 'react-native-live-audio-stream';
 
-const options = {
-    sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
-    channels: 1,        // 1 or 2, default 1
-    bitsPerSample: 16,  // 8 or 16, default 16
-    audioSource: 6,     // android only (see below)
-    bufferSize: 4096    // default is 2048
-};
 
 const styles = StyleSheet.create({
     container: {
         paddingTop: 35
     },
-    text: {
-        textAlign: "center",
-        fontSize: 20
+    button: {
+        width: 20
     }
 });
 
-const requestPerms = async () => {
+const requestRecordPermissions = async () => {
     try {
-        const granted = await PermissionsAndroid.request(PermissionsAndroid.RECORD_AUDIO, {
-            title: "Hey we need to use audio!",
-            message: "Please... ;-;",
-            buttonNeutral: "Maybe",
-            buttonNegative: "No",
-            buttonPositive: "Ok"
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        {
+          title: "App Record Permission",
+          message:
+            "Read App needs access to your microphone " +
+            "so you can use voice to text.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Initializing audio stream.");
+        const options = {
+            sampleRate: 16000, // default 44100
+            channels: 1, // 1 or 2, default 1
+            bitsPerSample: 16, // 8 or 16, default 16
+            audioSource: 6, // android only (see below)
+            bufferSize: 4096 * 2 // default is 2048
+        }
+
+        console.log(AudioRecord);
+        AudioRecord.init(options);
+        
+        AudioRecord.start();
+
+        AudioRecord.on('data', data => {
+            // base64-encoded audio data chunks
+            console.log("Stuff is happening!!!");
         });
-        if (granted) {
-            const options = {
-                sampleRate: 16000, // default 44100
-                channels: 1, // 1 or 2, default 1
-                bitsPerSample: 16, // 8 or 16, default 16
-                audioSource: 6, // android only (see below)
-                bufferSize: 4096 * 2 // default is 2048
-            };
-            AudioRecord.init(options);
-            AudioRecord.start();
-            LiveAudioStream.on('data', data => {
-                console.log("Something is happening!!!");
-            });
-        }
-        }
-        catch (err) {
-            console.log(err);
-        }
-}
+        AudioRecord.start();
+      } else {
+        console.log("Microphone permission denied.");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
 
 const VoiceTest = () => {
-    requestPerms();
+    requestRecordPermissions();
 
     return (
         <View style={styles.container}>
-            <Button title="Solid" type="solid" loading="true" />
+            <Button style={styles.container}
+                title="Solid" 
+                type="solid" 
+                loading="true" 
+            />
         </View>
     );
 }
