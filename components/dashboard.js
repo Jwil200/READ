159c8028795cad6627f5cf625fd325606404e2dd
Jose Lopez <jose.lopez.997@my.csun.edu>
2022-10-25 16:54:47 +0000

    Replace dashboard.js

--- components/dashboard.js
+++ components/dashboard.js
@@ -1,29 +1,52 @@
 // components/dashboard.js
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { StyleSheet, View, Text, FlatList, ScrollView } from 'react-native';
 import { Header, Divider, Tile } from "@rneui/themed";
-import BookTile from "./bookTile.js";
-import { bookData } from "./books.js";
+import BookTile from "../components/bookTile.js";
+import { bookData } from "../components/books.js";
+import Navbar from "../components/navbar";
 
 // Not using firebase at the moment, all content stored in books.js
 //import firebase from '../database/firebase';
 //require('firebase/auth')
 
 const styles = StyleSheet.create({
+  screen: {
+    height: '100%',
+    width: '100%',
+    paddingTop: 25,
+    backgroundColor: '#fff'
+  },
   container: {
     flex: 1,
     display: "flex",
     justifyContent: 'center',
     alignItems: 'center',
     padding: 10,
-    paddingTop: 35,
-    backgroundColor: '#fff'
+    paddingTop: 0
+  },
+  grid: {
+    width: '100%'
   },
   item: {
     width: "31.5%",
     alginItems: "center",
     margin: 3,
     backgroundColor: '#e9eef1'
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'left',
+    width: '100%',
+    paddingTop: 10
+  },
+  divider: {
+    width: '98%',
+    marginVertical: 5,
+  },
+  emptyText: {
+    color: "grey",
+    marginTop: 10
   }
 });
 
@@ -33,15 +56,86 @@ const Item = ({ item }) => (
   </View>
 );
 
-const Dashboard = () => {
+const ComponentItem = ({ item }) => (
+  <View>{item.jsx}</View>
+);
+
+const Dashboard = ({ navigation }) => {
+  let recentData = bookData.filter(e => e.isRecent);
+  let favoriteData = bookData.filter(e => e.isFavorite);
+
+  let componentList = [];
+
+  componentList.push({
+    _id: 1,
+    jsx: 
+      <View style={styles.container}>
+        <Text style={styles.title}>Recent</Text>
+        <Divider style={styles.divider} />
+        {
+          (recentData.length == 0)
+          ? <Text style={styles.emptyText}>You haven't read anything recently.</Text>
+          : <FlatList style={styles.grid}
+              data={recentData}
+              numColumns={3}
+              renderItem={Item}
+              keyExtractor={item => "r" + item._id}
+              listKey="r"
+            />
+        }
+      </View>
+  });
+  componentList.push({
+    _id: 2,
+    jsx: 
+      <View style={styles.container}>
+        <Text style={styles.title}>Favorites</Text>
+        <Divider style={styles.divider} />
+        {
+          (favoriteData.length == 0)
+          ? <Text style={styles.emptyText}>You don't have anything favorited.</Text>
+          : <FlatList style={styles.grid}
+              data={favoriteData}
+              numColumns={3}
+              renderItem={Item}
+              keyExtractor={item => "f" + item._id}
+              listKey="f"
+            />
+        }
+      </View>
+  });
+  componentList.push({
+    _id: 3,
+    jsx:
+      <View style={styles.container}>
+        <Text style={styles.title}>All</Text>
+        <Divider style={styles.divider} />
+        {
+          (bookData.length == 0)
+          ? <Text style={styles.emptyText}>You don't have any books in your library.</Text>
+          : <FlatList style={styles.grid}
+              data={bookData}
+              numColumns={3}
+              renderItem={Item}
+              keyExtractor={item => "a" + item._id}
+              listKey="a"
+            />
+        }
+      </View>
+  });
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={bookData}
-        numColumns={3}
-        renderItem={Item}
-        keyExtractor={item => item._id}
-      />
+    <View style={{flex: 1}}>
+      <View style={{flex: 0.9}}>
+        <FlatList
+          data={componentList}
+          renderItem={ComponentItem}
+          keyExtractor={item => item._id}
+        />
+      </View>
+      <View style={{flex: 0.1}}>
+        <Navbar nav={navigation}></Navbar>
+      </View>
     </View>
   );
 }
