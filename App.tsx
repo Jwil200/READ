5b7b4f354eb8ff9bd64be742dc3dba396d010647
grapes-cr <caitlinjrafael@gmail.com>
2022-11-03 11:09:14 -0700

    adding book store + search function

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 // App.js
 import * as React from 'react';
+import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './components/login';
@@ -8,6 +9,9 @@ import Dashboard from './components/dashboard';
 import Welcome from './components/afterSignup';
 import BookPreview from './components/bookPreview';
 import Settings from './components/settings';
+import Store from './components/store';
+import BookStorePreview from './components/bookStorePreview';
+import Cart from './components/cart.js';
 
 const Stack = createStackNavigator();
 
@@ -68,6 +72,33 @@ function MyStack() {
           title: 'Settings' ,
          headerLeft: null 
        }}
+      />
+      <Stack.Screen 
+       name="Store" 
+       component={Store} 
+       options= {({ navigation }) => ({
+        title: "Book Store", 
+          headerRight: () => (
+            <Button
+              onPress={() => navigation.navigate("Cart")}
+              title="Cart"
+              color="#fff"
+            />
+          )})}
+      />
+      <Stack.Screen 
+       name="BookStorePreview" 
+       component={BookStorePreview} 
+       options={{
+          title: 'Book Preview',
+       }}
+      />
+      <Stack.Screen 
+        name="Cart" 
+        component={Cart} 
+        options={{
+            title: 'Your Cart',
+        }}
       />
     </Stack.Navigator>
   );
