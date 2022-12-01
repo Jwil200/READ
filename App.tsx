ce6fa6da1c6662def82bff23d215d849ebdef465
Jose Lopez <jose.lopez.997@my.csun.edu>
2022-12-01 19:05:02 +0000

    Replace App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 // App.js
 import * as React from 'react';
+import { Button, View , Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Icon } from 'react-native-elements';
 import Login from './components/login';
 import Signup from './components/signup';
 import Dashboard from './components/dashboard';
 import Welcome from './components/afterSignup';
 import BookPreview from './components/bookPreview';
-import settings from './components/settings';
 import Settings from './components/settings';
+import Store from './components/store';
+import BookStorePreview from './components/bookStorePreview';
+import Cart from './components/cart.js';
+import TabBar from './components/TabBar';
+import FilterModal from './components/filterModal';
+
+
 
 const Stack = createStackNavigator();
 
@@ -34,42 +42,98 @@ function MyStack() {
       <Stack.Screen 
         name="Login" 
         component={Login} 
-        options={
-          {title: 'Login'},
-          {headerLeft: null} 
-        }
+        options={{
+          title: 'Login',
+          headerLeft: null, 
+        }}
       />
       <Stack.Screen 
        name="Dashboard" 
        component={Dashboard} 
-       options={
-         { title: 'Dashboard' },
-         {headerLeft: null} 
-       }
+       options= {({ navigation }) => ({
+        title: "Dashboard", 
+          headerRight: () => (
+            <Icon
+                style={{ paddingRight: 10}}
+                name='filter-alt'
+                color='#fff' 
+            />
+          )})}
       />
       <Stack.Screen 
        name="Welcome" 
        component={Welcome} 
-       options={
-         { title: 'Welcome' },
-         {headerLeft: null} 
-       }
+       options={{
+          title: 'Welcome to READ!' ,
+          headerLeft: null
+       }}
       />
       <Stack.Screen 
        name="BookPreview" 
        component={BookPreview} 
-       options={
-         { title: 'Book Preview' },
-         {headerLeft: null} 
-       }
+       options={{
+          title: 'Book Preview',
+       }}
       />
+      <Stack.Screen
+       name="Tabbar" 
+       component={TabBar} 
+       options={{
+        headerTitle: () => (
+          <Image style={{ width:150, height: 150, resizeMode:'contain', position:'relative'}} source={require("./assets/read-logo.png")} />
+        ),
+        headerLeft: null
+       }}
+       />
       <Stack.Screen 
        name="Settings" 
        component={Settings} 
-       options={
-         { title: 'Settings' },
-         {headerLeft: null} 
-       }
+       options={{
+          title: 'Settings' ,
+         headerLeft: null 
+       }}
+      />
+       <Stack.Group screenOptions={{ presentation: 'modal' }}>
+        <Stack.Screen 
+        name="FilterModal"
+        component={FilterModal}
+        options={{
+          title: null,
+         headerLeft: null
+         }}  />
+      </Stack.Group>
+      <Stack.Screen 
+       name="Store" 
+       component={Store} 
+       options= {({ navigation }) => ({
+        title: "Book Store", 
+          headerRight: () => (
+            <View style={{ flexDirection:"row", padding: 10}}>
+            <Icon
+                style={{ paddingRight: 10}}
+                name='filter-alt'
+                color='#fff' 
+            />
+            <Icon
+              name='shopping-cart'
+              onPress={() => navigation.navigate("Cart")}
+              color='#fff' />
+            </View>
+          )})}
+      />
+      <Stack.Screen 
+       name="BookStorePreview" 
+       component={BookStorePreview} 
+       options={{
+          title: 'Book Preview',
+       }}
+      />
+      <Stack.Screen 
+        name="Cart" 
+        component={Cart} 
+        options={{
+            title: 'Your Cart',
+        }}
       />
     </Stack.Navigator>
   );
@@ -78,6 +142,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <MyStack />
+
     </NavigationContainer>
   );
 }
