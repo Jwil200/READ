9d959ba86e0d988ecff919f82fbd0775373be857
Jose Lopez <jose.lopez.997@my.csun.edu>
2023-03-02 20:02:47 +0000

    changed settings page to avoid array mapping issue, working on darkmode

--- pages/settings.js
+++ pages/settings.js
@@ -1,187 +1,132 @@
-import React, { Component,useState  } from 'react';
-import { StyleSheet, View, Text, Image, FlatList, ImageStore, LogBox, ImageBackground, useColorScheme, ScrollView} from 'react-native';
-import { Header, Divider, Tile, Switch} from "@rneui/themed";
-import { Button } from 'react-native-elements/dist/buttons/Button.js';
-import { useNavigation, NavigationContainer, Darktheme } from '@react-navigation/core';
-import { colors, SearchBar} from 'react-native-elements';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Switch, TouchableOpacity } from 'react-native';
+import {useColorScheme} from 'react-native';
+import { Image } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
-const styles = StyleSheet.create({
-  colorchange: {
-    backgroundColor:'white'
-  },
-  tinylogo:{
-      width:35,
-      height:35,
-      flex:1,
-      position:"absolute",
-      marginVertical: 10,
-      marginLeft: 20
-  },
-  tinylogo2:{
-    width:50,
-    height:50,
-    flex:1,
-    position:"absolute",
-    marginVertical: 60,
-    marginLeft: 10
-},
-tinylogo3:{
-  width:50,
-  height:50,
-  flex:1,
-  position:"absolute",
-  marginVertical: 120,
-  marginLeft: 10
-},
-tinylogo4:{
-  width:50,
-  height:50,
-  flex:1,
-  position:"absolute",
-  marginVertical: 180,
-  marginLeft: 10
-},
-tinylogo5:{
-  width:45,
-  height:45,
-  flex:1,
-  position:"absolute",
-  marginVertical: 410,
-  marginLeft: 15
-},
-tinylogo6:{
-  width:50,
-  height:50,
-  flex:1,
-  position:"absolute",
-  marginVertical: 520,
-  marginLeft: 10
-},
-tinylogo7:{
-  width:50,
-  height:50,
-  flex:1,
-  position:"absolute",
-  marginVertical: 460,
-  marginLeft: 10
-},
-tinylogo8:{
-  width:40,
-  height:40,
-  flex:1,
-  position:"absolute",
-  marginVertical: 240,
-  marginLeft: 15
-},
-tinylogo9:{
-  width:40,
-  height:40,
-  flex:1,
-  position:"absolute",
-  marginVertical: 300,
-  marginLeft: 15
-},
-  toggle:{
-    marginLeft: 300,
-    marginVertical:240,
-    flex:2,
-    position:'absolute',
-    zIndex:999
-  },
-  toggle2:{
-    marginLeft: 300,
-    marginVertical:300,
-    flex:2,
-    position:'absolute',
-    zIndex:999
-  }
-});
+const SettingsScreen = ({navigation}) => {
+  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+  const colorScheme = useColorScheme ();
+  const isSystemDarkModeEnabled = colorScheme === 'dark';
+  useEffect(() => {
+    setDarkModeEnabled(isSystemDarkModeEnabled);
+  }, [isSystemDarkModeEnabled]);
+  
+
+  const toggleDarkMode = () => {
+    setDarkModeEnabled(!darkModeEnabled);
+  };
 
 
-const Settings = ({navigation}) => {
-    const settingOptions=[
-    {title: "Account Settings"},
-    {title: "Purchase Settings"},
-    {title: "Audio Settings"},
-    {title: "Accessibility"},
-    {title: "Read-Only Mode"},
-    {title: "Dark Mode"},
-    {},
-    {title: "Parental Controls"}, 
-    {title: "Help and Support"}, 
-    {title: "Contact Us"},
-    ]
   return (
-    <ScrollView>
-    <View style={styles.colorchange}>
- 
+    <ScrollView style={[styles.container, darkModeEnabled ? styles.darkMode : styles.lightMode]}>
+      <View style={styles.itemContainer}>
+      <Image source={require('../assets/darkmode.png')}
+        style={styles.tinylogo}
+       /> 
+        <Text style={[styles.itemTitle, { color: darkModeEnabled ? '#fff' : '#000' }]}>Dark Mode</Text>
+        <Switch style={styles.switchloco} value={darkModeEnabled} onValueChange={toggleDarkMode} />
+      </View>
+      <View style={styles.itemContainer}>
+      <Image source={require('../assets/parents.png')}
+        style={styles.tinylogo}
+       /> 
+        <Text style={[styles.itemTitle, { color: darkModeEnabled ? '#fff' : '#000' }]}>Parental Controls</Text>
+      </View>
+      <View style={styles.itemContainer}>
+      <Image source={require('../assets/man.png')}
+        style={styles.tinylogo}
+       /> 
+        <Text style={[styles.itemTitle, { color: darkModeEnabled ? '#fff' : '#000' }]}>Accessibility</Text>
+      </View>
+      <View style={styles.itemContainer}>
       <Image source={require('../assets/settingsicon.png')}
         style={styles.tinylogo}
-       />
-      <Image source={require('../assets/bag.jpg')}
-        style={styles.tinylogo2}
-       />
-      <Image source={require('../assets/sound.png')}
-        style={styles.tinylogo3}
-       />
-      <Image source={require('../assets/man.png')}
-        style={styles.tinylogo4}
-       />
-      <Image source={require('../assets/parents.png')}
-        style={styles.tinylogo5}
-       />
-      <Image source={require('../assets/phone.png')}
-        style={styles.tinylogo6}
-       />
-      <Image source={require('../assets/question.png')}
-        style={styles.tinylogo7}
-       />       
-      <Image source={require('../assets/book.png')}
-        style={styles.tinylogo8}
-       />    
-      <Image source={require('../assets/darkmode.png')}
-        style={styles.tinylogo9}
        /> 
-      <Switch style={styles.toggle}>
-
-         </Switch>
-      <Switch style={styles.toggle2}>
-        
-
-      </Switch>
-        {settingOptions.map(({title})=>
-        <View key={title}>
-            <View style={{
-                paddingHorizontal:20,
-                paddingBottom:20,
-                paddingTop:20,
-                paddingLeft:75
-                      }
-            }>
-            <Text style={{fontSize:17}}>{title}</Text>
-
-            </View>
-            <View style={{height: 0.5, backgroundColor:colors.grey0}} />
-        </View>)}
-        <Button 
-        title="Sign Out"
-        titleStyle={{ fontWeight: '500' }}
-        buttonStyle={{
-        backgroundColor: 'blue',
-        borderColor: 'transparent',
-        borderWidth: 0,
-        margin:160,
-        alignSelf: "auto",
-        bottom:120
-        }}
-         onPress={() => navigation.navigate('Login')}
-         />
-        <Text style={{bottom:400,margin:160,fontSize:17}}> {'\u00A9'}Read 2022 </Text>
-
-    </View>
+        <Text style={[styles.itemTitle, { color: darkModeEnabled ? '#fff' : '#000' }]}>Account Settings</Text>
+      </View>
+      <View style={styles.itemContainer}>
+      <Image source={require('../assets/bag.jpg')}
+        style={styles.tinylogo}
+       /> 
+        <Text style={[styles.itemTitle, { color: darkModeEnabled ? '#fff' : '#000' }]}>Purchase Settings</Text>
+      </View>
+      <View style={styles.itemContainer}>
+      <Image source={require('../assets/question.png')}
+        style={styles.tinylogo}
+       /> 
+        <Text style={[styles.itemTitle, { color: darkModeEnabled ? '#fff' : '#000' }]}>Help and Support</Text>
+      </View>
+      <View style={styles.itemContainer}>
+      <Image source={require('../assets/phone.png')}
+        style={styles.tinylogo}
+       /> 
+        <Text style={[styles.itemTitle, { color: darkModeEnabled ? '#fff' : '#000' }]}>Contact Us</Text>
+      </View>
+      <TouchableOpacity style={styles.signOutButton}
+                         onPress={() => navigation.navigate('Login')}> 
+        <Text style={styles.signOutButtonText}>Sign Out</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  darkMode: {
+    backgroundColor: '#222',
+
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: "flex-start",
+    alignItems: 'center',
+    paddingVertical: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  itemTitle: {
+    fontSize: 18,
+  },
+  signOutButton: {
+    backgroundColor: '#f00',
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginTop: 30,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  signOutButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  tinylogo:{
+    width:30,
+    height:30,
+    position:"relative",
+    marginLeft: 10,
+    marginRight: 20,
+    marginTop: 0,
+    resizeMode: "center",
+    paddingVertical: 10,
   
-      
-} 
-export default Settings;
+  },
+  switchloco:{
+    position: "relative",
+    marginLeft: 150
+  
+  },
+});
+
+export default SettingsScreen;
