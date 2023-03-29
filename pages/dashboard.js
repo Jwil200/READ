e98aaaa5fded5e21a51a147dc4207d5c10388374
Jose Lopez <jose.lopez.997@my.csun.edu>
2023-03-29 02:38:06 +0000

    Replace dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,143 +1,253 @@
-// components/dashboard.js
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, ScrollView } from 'react-native';
-import { Header, Divider, Tile } from "@rneui/themed";
-import BookTile from "../components/bookTile.js";
-import { SearchBar } from 'react-native-elements';
-import { bookData } from "../assets/books.js";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { Header, Divider, Tile } from '@rneui/themed';
+import BookTile from '../components/bookTile.js';
+import { SearchBar, Icon } from 'react-native-elements';
+import { bookData } from '../assets/books.js';
 
-// Not using firebase at the moment, all content stored in books.js
-//import firebase from '../database/firebase';
-//require('firebase/auth')
+
+
 
 const styles = StyleSheet.create({
   screen: {
     height: '100%',
     width: '100%',
     paddingTop: 25,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   container: {
     flex: 1,
-    display: "flex",
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     padding: 10,
-    paddingTop: 0
+    paddingTop: 0,
   },
   grid: {
-    width: '100%'
+    width: '100%',
   },
   item: {
-    width: "31.5%",
-    alginItems: "center",
+    width: '31.5%',
+    alignItems: 'center',
     margin: 3,
-    backgroundColor: '#e9eef1'
+    backgroundColor: '#e9eef1',
   },
   title: {
     fontSize: 24,
     textAlign: 'left',
     width: '100%',
-    paddingTop: 10
+    paddingTop: 10,
   },
   divider: {
     width: '98%',
     marginVertical: 5,
   },
   emptyText: {
-    color: "grey",
-    marginTop: 10
-  }
+    color: 'grey',
+    marginTop: 10,
+  },
+  favoriteIcon: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+  },
 });
 
-const Item = ({ item }) => (
+
+
+
+const Item = ({ item, updateFavorites }) => (
   <View style={styles.item}>
-    <BookTile 
-     key={"i" + item._id} 
-     id={item._id}
-     title= {item.title}
-     progress={item.progress}
-     isRecent={item.isRecent}
-     isFavorite={item.isFavorite}
- />
+    <BookTile
+      key={'i' + item._id}
+      id={item._id}
+      title={item.title}
+      progress={item.progress}
+      isRecent={item.isRecent}
+      isFavorite={item.isFavorite}
+    />
+    <TouchableOpacity
+      style={styles.favoriteIcon}
+      onPress={() => updateFavorites(item._id)}
+    >
+      <Icon
+        name={item.isFavorite ? 'heart' : 'heart-o'}
+        type='font-awesome'
+        color={item.isFavorite ? 'red' : 'grey'}
+      />
+    </TouchableOpacity>
   </View>
 );
 
-const ComponentItem = ({ item }) => (
-  <View>{item.jsx}</View>
-);
+
+
+
+const ComponentItem = ({ item }) => <View>{item.jsx}</View>;
+
+
+
 
 const Dashboard = ({ navigation }) => {
-  let recentData = bookData.filter(e => e.isRecent);
-  let favoriteData = bookData.filter(e => e.isFavorite);
+  let recentData = bookData.filter((e) => e.isRecent);
+  let favoriteData = bookData.filter((e) => e.isFavorite);
+
+
+
 
   let componentList = [];
 
+
+
+
   const [search, setSearch] = useState('');
 
-  componentList.push({
-    _id: 1,
-    jsx: 
-      <View style={styles.container}>
-        <Text style={styles.title}>Recent</Text>
-        <Divider style={styles.divider} />
-        {
-          (recentData.length == 0)
-          ? <Text style={styles.emptyText}>You haven't read anything recently.</Text>
-          : <FlatList style={styles.grid}
-              data={recentData}
-              numColumns={3}
-              renderItem={Item}
-              keyExtractor={item => "r" + item._id}
-              listKey="r"
-            />
-        }
-      </View>
-  });
-  componentList.push({
-    _id: 2,
-    jsx: 
+
+
+
+  const updateFavorites = (bookId) => {
+    const updatedBookData = bookData.map((book) => {
+      if (book._id === bookId) {
+        book.isFavorite = !book.isFavorite;
+      }
+      return book;
+    });
+
+
+
+
+    const updatedFavoriteData = updatedBookData.filter(
+      (book) => book.isFavorite,
+    );
+
+
+
+
+    componentList[1].jsx = (
       <View style={styles.container}>
         <Text style={styles.title}>Favorites</Text>
         <Divider style={styles.divider} />
-        {
-          (favoriteData.length == 0)
-          ? <Text style={styles.emptyText}>You don't have anything favorited.</Text>
-          : <FlatList style={styles.grid}
-              data={favoriteData}
-              numColumns={3}
-              renderItem={Item}
-              keyExtractor={item => "f" + item._id}
-              listKey="f"
-            />
-        }
+        {updatedFavoriteData.length === 0 ? (
+          <Text style={styles.emptyText}>
+            You don't have anything favorited.
+          </Text>
+        ) : (
+          <FlatList
+            style={styles.grid}
+            data={updatedFavoriteData}
+            numColumns={3}
+            renderItem={({ item }) => (
+              <Item item={item} updateFavorites={updateFavorites} />
+            )}
+            keyExtractor={(item) => 'f' + item._id}
+            listKey='f'
+          />
+        )}
       </View>
+    );
+
+
+
+
+    setFilteredDataSource([...componentList]);
+  };
+
+
+
+
+  componentList.push({
+    _id: 1,
+    jsx: (
+      <View style={styles.container}>
+        <Text style={styles.title}>Recent</Text>
+        <Divider style={styles.divider} />
+        {recentData.length === 0 ? (
+          <Text style={styles.emptyText}>
+            You haven't read anything recently.
+          </Text>
+        ) : (
+          <FlatList
+            style={styles.grid}
+            data={recentData}
+            numColumns={3}
+            renderItem={({ item }) => (
+              <Item item={item} updateFavorites={updateFavorites} />
+            )}
+            keyExtractor={(item) => 'r' + item._id}
+            listKey='r'
+          />
+        )}
+      </View>
+    ),
+  });
+  componentList.push({
+    _id: 2,
+    jsx: (
+      <View style={styles.container}>
+        <Text style={styles.title}>Favorites</Text>
+        <Divider style={styles.divider} />
+        {favoriteData.length === 0 ? (
+          <Text style={styles.emptyText}>
+            You don't have anything favorited.
+          </Text>
+        ) : (
+          <FlatList
+            style={styles.grid}
+            data={favoriteData}
+            numColumns={3}
+            renderItem={({ item }) => (
+              <Item item={item} updateFavorites={updateFavorites} />
+            )}
+            keyExtractor={(item) => 'f' + item._id}
+            listKey='f'
+          />
+        )}
+      </View>
+    ),
   });
   componentList.push({
     _id: 3,
-    jsx:
+    jsx: (
       <View style={styles.container}>
         <Text style={styles.title}>All</Text>
         <Divider style={styles.divider} />
-        {
-          (bookData.length == 0)
-          ? <Text style={styles.emptyText}>You don't have any books in your library.</Text>
-          : <FlatList style={styles.grid}
-              data={bookData}
-              numColumns={3}
-              renderItem={Item}
-              keyExtractor={item => "a" + item._id}
-              listKey="a"
-            />
-        }
+        {bookData.length === 0 ? (
+          <Text style={styles.emptyText}>
+            You don't have any books in your library.
+          </Text>
+        ) : (
+          <FlatList
+            style={styles.grid}
+            data={bookData}
+            numColumns={3}
+            renderItem={({ item }) => (
+              <Item item={item} updateFavorites={updateFavorites} />
+            )}
+            keyExtractor={(item) => 'a' + item._id}
+            listKey='a'
+          />
+        )}
       </View>
+    ),
   });
+
+
+
 
   const [filteredDataSource, setFilteredDataSource] = useState(componentList);
 
+
+
+
   const searchFilterFunction = (text) => {
     if (text) {
-      const newData = bookData.filter(function (item) { //Create an array of newData that filters library data
+      const newData = bookData.filter(function (item) {
         const itemData = item.title
           ? item.title.toUpperCase()
           : ''.toUpperCase();
@@ -145,57 +255,89 @@ const Dashboard = ({ navigation }) => {
         return itemData.indexOf(textData) > -1;
       });
 
+
+
+
       const filteredList = [];
 
-      filteredList.push({ //Populate the filtered list with the book componenets
+
+
+
+      filteredList.push({
         _id: 1,
-        jsx:
-        <View style={styles.container}>
-        {
-          (newData.length == 0)
-          ? <Text style={styles.emptyText}>Book not available.</Text>
-          : <FlatList style={styles.grid}
-              data={newData}
-              numColumns={3}
-              renderItem={Item}
-              keyExtractor={item => "a" + item._id}
-              listKey="a"
-            />
-        }
-      </View>
+        jsx: (
+          <View style={styles.container}>
+            {newData.length === 0 ? (
+              <Text style={styles.emptyText}>Book not available.</Text>
+            ) : (
+              <FlatList
+                style={styles.grid}
+                data={newData}
+                numColumns={3}
+                renderItem={({ item }) => (
+                  <Item item={item} updateFavorites={updateFavorites} />
+                )}
+                keyExtractor={(item) => 'a' + item._id}
+                listKey='a'
+              />
+            )}
+          </View>
+        ),
       });
-      setFilteredDataSource(filteredList); //Update state variables
+      setFilteredDataSource(filteredList);
       setSearch(text);
     } else {
-      setFilteredDataSource(componentList); //On clear, go back to og component list
+      setFilteredDataSource(componentList);
       setSearch(text);
     }
   };
 
+
+
+
   return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 0.9}}>
-      <SearchBar round 
-          cancelButtonTitle='X'
-          lightTheme
-          searchIcon={{ size: 20 }}
-          inputStyle={{backgroundColor: 'white'}}
-          placeholderTextColor={'#g5g5g5'}
-          placeholder={'Search'}
-          inputContainerStyle={{ 
-            backgroundColor: 'white',
-          }} 
-          value={search}
-          onChangeText={(text) => searchFilterFunction(text)}
-          onClear={() => searchFilterFunction('')}
-        />
+    <View style={styles.screen}>
+      <Header
+        containerStyle={{
+          backgroundColor: '#fff',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          marginTop: -95
+        }}
+      />
+      <SearchBar
+        round
+        searchIcon={{ size: 24 }}
+        onChangeText={(text) => searchFilterFunction(text)}
+        onClear={() => searchFilterFunction('')}
+        placeholder='Search books...'
+        value={search}
+        containerStyle={{ backgroundColor: 'transparent' }}
+        inputContainerStyle={{ backgroundColor: '#e9eef1' }}
+        inputStyle={{ color: 'black' }}
+      />
+      <ScrollView style={{ width: '100%', marginTop: 10 }}>
         <FlatList
           data={filteredDataSource}
-          renderItem={ComponentItem}
-          keyExtractor={item => item._id}
+          renderItem={({ item }) => <ComponentItem item={item} />}
+          keyExtractor={(item) => 'c' + item._id}
+          listKey='c'
         />
-      </View>
+      </ScrollView>
     </View>
   );
-}
+};
+
+
+
+
 export default Dashboard;
+
+
+
+
+
+
+
+
+
