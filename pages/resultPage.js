1013b1656bdec8ee143b5017550dd848eb8cf85b
Oscar Ulloa <OscarTR10@Oscars-MacBook-Air.local>
2023-05-07 16:23:43 -0700

    final resultPage changes

--- pages/resultPage.js
+++ pages/resultPage.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import { View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import Sound from 'react-native-sound';
 import OrangeButton from '../assets/orangeButton';
 import PerfectScoreAnimation from '../components/perfectScoreAnimation';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
+import { Divider, useTheme } from '@rneui/themed';
 
 
 const ResultPage = ({route, navigation}) => {//props) => {
@@ -19,10 +20,10 @@ const ResultPage = ({route, navigation}) => {//props) => {
 
     const db = firestore();
     const currentUid = auth().currentUser.uid;
+    const userRef = db.collection('Users').doc(currentUid).collection("Stats").doc("Stats");
+    const bookref = db.collection('Users').doc(currentUid).collection("Library").doc(results.bookTitle);
 
     const updateStats = async () => {//update all book related stats
-        const userRef = await db.collection('Users').doc(currentUid).collection("Stats").doc("Stats");
-        const bookref = await db.collection('Users').doc(currentUid).collection("Library").doc(results.bookTitle);
 
         const updatedProgress = (sentencesRead / total);
 
@@ -81,37 +82,53 @@ const ResultPage = ({route, navigation}) => {//props) => {
     }, []);
 
   return (
-    <View style={styles.container}>
-
-        
-      <ImageBackground
+    <View style={styles.container}>    
+    <ImageBackground
         source={{uri: bookCover}} // use a png/jpg or a url 
         style={styles.backgroundImage}
-      >
+    >
+        
         <View style={styles.contentContainer}>
-        <Text style={styles.titleText}>Completion </Text>
-          <View style={styles.progressContainer}>
+          <Text style={styles.bookTitle}>{results.bookTitle}</Text>    
+          <Text style={styles.titleText}>Completion:</Text>
+                <AnimatedCircularProgress
+                    size={150}
+                    width={15}
+                    fill={progress}
+                    tintColor="lightgreen" // Blue color for correct words
+                    back
+                    backgroundColor="grey" // Red color for incorrect words
+                    backgroundWidth = {15}
+                    lineCap="round"
+                    rotation={0}
+                arcSweepAngle={360}
+                >
+                {(fill) => <Text style={styles.progressText}>{Math.round(progress)}%</Text>}
+                </AnimatedCircularProgress>
+            <Text style={styles.titleText}>Results:</Text>
 
             <AnimatedCircularProgress
-              size={120}
-              width={8}
-              fill={progress}
-              tintColor="#339af0" // Blue color for correct words
-              backgroundColor="#ff4d4f" // Red color for incorrect words
-              lineCap="round"
-              rotation={0}
-              arcSweepAngle={360}
-            >
-              {(fill) => <Text style={styles.progressText}>{Math.round(progress)}%</Text>}
-            </AnimatedCircularProgress>
-          </View>
-        <Text style={styles.titleText}>Results</Text>
+                    size={150}
+                    width={15}
+                    fill={progress}
+                    tintColor="blue" // Blue color for correct words
+                    back
+                    backgroundColor="red" // Red color for incorrect words
+                    backgroundWidth = {15}
+                    lineCap="round"
+                    rotation={0}
+                arcSweepAngle={360}
+                >
+                {(fill) => <Text style={styles.progressText}>{Math.round(progress)}%</Text>}
+                </AnimatedCircularProgress>
+
+    
             
-        <Text style={styles.corectWordsText}>Correct Sentences: {sentencesCorrect}</Text>
-        <Text style={styles.incorrectWordsText}>Incorrect Sentences: {sentencesIncorrect}</Text>
-        <Text style={styles.timeReadText}>Sentences Read: {sentencesRead}</Text>
+                <Text style={styles.corectWordsText}>Correct Sentences: {sentencesCorrect}</Text>
+                <Text style={styles.incorrectWordsText}>Incorrect Sentences: {sentencesIncorrect}</Text>
+                <Text style={styles.timeReadText}>Sentences Read: {sentencesRead}</Text>
         {
-          (sentencesIncorrect === 0) ? <PerfectScoreAnimation visible={true}/> : ""
+        (sentencesIncorrect === 0) ? <PerfectScoreAnimation visible={true}/> : ""
         }
 
         <View style={styles.buttonContainer}>
@@ -121,36 +138,76 @@ const ResultPage = ({route, navigation}) => {//props) => {
 
         </View>
 
-      </ImageBackground>
-
-     
-    </View>
+    </ImageBackground>
+    </View> 
   );
 };
 
 const styles = StyleSheet.create({
     titleText: {
-        fontFamily: 'serif', // 'serif', 'sans-serif', 'monospace', 'cursive', 'fantasy
+        fontFamily: 'monospace', // 'serif', 'sans-serif', 'monospace', 'cursive', 'fantasy
         fontSize: 20,
+        color: 'black',
+        textAlign: 'center',
+
+    },
+    bookTitle: {
+        fontFamily: 'fantasy', // 'serif', 'sans-serif', 'monospace', 'cursive', 'fantasy
+        fontSize: 30,
+        color: 'black',
+        textAlign: 'center',
+    },
+    subTitleText: {
+        fontFamily: 'monospace', // 'serif', 'sans-serif', 'monospace', 'cursive', 'fantasy
+        fontSize: 15,
         color: 'black',
         textAlign: 'center',
     },
     container: {
         flex: 1,
-    },
-    backgroundImage: {
-        flex: 1,
+        backgroundColor: 'white',
         width: '100%',
-        height: '25%',
-        resizeMode: 'contain',
+        height: '100%',
     },
+
     contentContainer: {
+        top: 50,
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
+    divider: {
+        width: '100%',
+        marginVertical: 5,
+      },
+      divider2: {
+        top: -100,
+        width: '100%',
+        marginVertical: 5,
+      },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '20%',
+        resizeMode: "contain",
+    },
+    resultContainer: {
+        flex: 1,
+        top: -180,
+        justifyContent: 'center',
+
+    },
+    scrollView: {
+        flex: 1,
+        backgroundColor: 'white',
+        width: '100%',
+        height: '100%',
+    },
     progressContainer: {
-      marginVertical: 16,
+        top: -50,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     progressText: {
         fontSize: 20,
@@ -192,6 +249,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginVertical: 16,
+        flex: 1,
     },
     dividerLine: {
         flex: 1,
@@ -207,6 +265,9 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: 'black',
         marginVertical: 8,
+    },
+    buttonContainer: {
+        top: 0,
     },
 
   });
